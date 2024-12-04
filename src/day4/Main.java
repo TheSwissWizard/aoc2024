fa5701de3910881f3bf617e541a72d43724f6145
Florian Hug <florian.hug.98@gmail.com>
2024-12-04 15:07:41 +0100

    emove inputs from git

---
+++ src/day4/Main.java
@@ -0,0 +1,24 @@
+package day4;
+
+import until.InputReader;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        List<String> input = InputReader.getLines(4);
+    }
+
+    public static int partOne(List<String> lines) {
+        int result = 0;
+
+        
+
+
+
+
+        return 0;
+    }
+}
